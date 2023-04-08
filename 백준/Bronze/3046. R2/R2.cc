618a61d847d411cd719cd072b47ba50f57d5d93e
Sail <sail@kakao.com>
2023-04-08 14:00:28 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio();
+    cin.tie();
+    
+    int R1, R2, S;
+    
+    cin >> R1 >> S;
+    R2 = 2*S-R1;
+    
+    cout << R2;
+    
+    return 0;
+}
