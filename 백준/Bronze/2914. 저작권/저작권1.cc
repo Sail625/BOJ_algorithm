c6dfea9b572c233c2afbfc07cab4cf0b3a6937f6
Sail <sail@kakao.com>
2023-04-10 22:19:18 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, I, melody;
+    
+    cin >> A >> I;
+    
+    melody = A*(I-1)+1;
+    cout << melody;
+    
+    return 0;
+}
