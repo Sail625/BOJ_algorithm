bcdd9044c12f65ab4185de25271d2178bb3a1dcc
Sail <sail@kakao.com>
2023-05-16 23:04:43 +0900

    Update 평균은 넘겠지.cc
    변수명 변경(array → score)

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.cc
@@ -3,12 +3,12 @@
 //백준 4344 : 평균은 넘겠지
 
 //평균
-double avg(int N, const int* array) { 
+double avg(int N, const int* score) {
     int sum = 0;
     double average = 0;
 
     for (int i = 0; i < N; i++) {
-        sum += array[i];
+        sum += score[i];
     }
 
     average = (double) sum / N;
@@ -16,14 +16,14 @@ double avg(int N, const int* array) {
 }
 
 //평균을 넘는 학생 비율
-double find_rate(double avg, const int* array, int N) { 
+double find_rate(double avg, const int* score, int N) {
     int count = 0;
     for (int i = 0; i < N; i++) {
-        if (avg < array[i]) {
+        if (avg < score[i]) {
             count++;
         }
     }
-    return (double)count / N*100;
+    return (double)count / N * 100;
 }
 
 
@@ -33,18 +33,18 @@ int main() {
 
     std::cin >> C;
 
-    int* array;
+    int* score;
 
     for (int i = 0; i < C; i++) {
         std::cin >> N;
-        array = new int[N];
+        score = new int[N];
         for (int j = 0; j < N; j++) {
-            std::cin >> array[j];
+            std::cin >> score[j];
         }
 
         std::cout << std::fixed;
         std::cout.precision(3);
-        std::cout << find_rate(avg(N, array), array, N) << "%\n";
-        delete[] array;
+        std::cout << find_rate(avg(N, score), score, N) << "%\n";
+        delete[] score;
     }
 }
